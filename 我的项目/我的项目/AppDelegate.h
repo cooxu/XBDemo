88f7b6cfbae684ae6fbc60a98a1f6d4b6afe4f6c
xubo <xubo@fang.com>
2017-05-24 08:30:13 +0800

    初始化项目

---
+++ 我的项目/我的项目/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  我的项目
+//
+//  Created by 许博 on 2017/5/24.
+//  Copyright © 2017年 mechat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 我的项目/我的项目/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  我的项目
+//
+//  Created by 许博 on 2017/5/24.
+//  Copyright © 2017年 mechat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
